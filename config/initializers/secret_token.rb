6a9c57a3022543d91e18a3fc2b1baa990d395efb
Jane Sternbach <janesternbach@gmail.com>
2013-08-21 17:25:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watchr::Application.config.secret_token = '7bdeeddf4a7b2ffda2a689e94525b54a3ecb8f6e580502a2528e888c0a08458e38de9f0a5cbb0fa688de767c8d8474cd8d5d9c9965090fe877aa9ba8eae02e2a'
