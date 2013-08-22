8e4eda03451f0c1cc92eda83c87e9dbd91d65f5e
Jane Sternbach <janesternbach@gmail.com>
2013-08-22 07:04:23 -0400

    scrolling working!

---
+++ app/assets/javascripts/watchr.js
@@ -0,0 +1,43 @@
+$(function(){
+  set_button_heights();
+  $("body").on("mouseenter", ".slide-right", start_timer);
+  $("body").on("mouseleave", ".slide-right", stop_timer);
+  $("body").on("mouseenter", ".slide-left", start_timer);
+  $("body").on("mouseleave", ".slide-left", stop_timer);
+
+
+})
+x = {in_theaters:0, opening:0, upcoming:0, following:0}
+timer = null;
+el = null;
+left = null;
+total = null;
+
+function start_timer() {
+  el = this.parentElement.children[2];
+  left = this.classList[0].split("-")[1] == "left"
+  total = $(this).siblings(".table-holder").children().children().children().width();
+  timer = setInterval(scrollSideways, 20);
+}
+
+function stop_timer() {
+  clearInterval(timer);
+}
+function scrollSideways() {
+  if(left && x[el.id] > 0 ) {
+    x[el.id] -= 10;
+  }
+  else if(!left && x[el.id] < total) {
+    x[el.id] += 10;
+  }
+  $(el).scrollLeft(x[el.id]);
+}
+
+function set_button_heights() {
+  // $.each($(".table-holder"), function(k, v){
+  //   var value = $(v);
+  //   var height = value.height();
+  //   console.log(height);
+  //   value.siblings().css("height", height+"px");
+  // })//end each loop
+}//end set_button_heights function
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
     # Movie.get_movies
     movies = Movie.all
     # ["box_office", "in_theaters", "opening", "upcoming"]
-    @in_theaters = movies.select{|x| x.kind == "in_theaters"}
-    @box_office = movies.select{|x| x.kind == "box_office"}
-    @opening = movies.select{|x| x.kind == "opening"}
-    @upcoming = movies.select{|x| x.kind == "upcoming"}
+    @in_theaters = movies.select{|x| x.kind == "in_theaters"}.sort_by{|x| (x.a_score+x.c_score)/2}.reverse
+    @box_office = movies.select{|x| x.kind == "box_office"}.sort_by{|x| (x.a_score+x.c_score)/2}.reverse
+    @opening = movies.select{|x| x.kind == "opening"}.sort_by{|x| (x.a_score+x.c_score)/2}.reverse
+    @upcoming = movies.select{|x| x.kind == "upcoming"}.sort_by{|x| (x.a_score+x.c_score)/2}.reverse
   end
 end
